e7e84aa83a2a3dac75a1460b2980d2bf829b9ea7
Pranjal <pranjalnegi82@gmail.com>
2026-07-28 04:22:20 +0530

    db new

--- db.js
+++ db.js
@@ -14,6 +14,7 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'qwertyui',
   database: process.env.DB_NAME || 'exam_seating',
